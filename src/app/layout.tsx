a2a6ccc3e139c98af92ce6edbb9732acb870d574
danibystrom <124156950+danibystrom@users.noreply.github.com>
2024-08-28 10:11:06 +0200

    Trying to add backroundimg and logo to banner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,11 @@
+"use client";
 import { ThemeProvider } from "@emotion/react";
 import { CssBaseline } from "@mui/material";
-import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import theme from "./theme";
 
 const inter = Inter({ subsets: ["latin"] });
-
-export const metadata: Metadata = {
-  title: "Ciliegia Grappa",
-};
 
 export default function RootLayout({
   children,
